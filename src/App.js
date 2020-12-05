78075cfed9b1d8869ee15256eb80a1a31faf6793
dialectic4th <hiromuber@gmail.com>
2020-12-05 05:04:10 +0000

    write JSX and Javascirpt

--- src/App.js
+++ src/App.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//JSX
+// class App extends Component {
+//   render() {
+//     return <div>Hello, world!<>;
+//   }
+// }
+
+//Javascript
 class App extends Component {
   render() {
-    return <div>Hello, world!</div>;
+    return React.createElement(
+        "div",
+        null,
+        "Hello world"
+    )
   }
 }
 
